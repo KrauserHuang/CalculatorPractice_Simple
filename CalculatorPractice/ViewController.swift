a4d3a47df85e90456b037f70a71bf3230ebdf935
krauserhuang <crescentdence@gmail.com>
2020-09-23 14:01:57 +0800

    Initial Commit

---
+++ CalculatorPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorPractice
+//
+//  Created by Tai Chin Huang on 2020/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
